6fee96027bd30d4835fc0c26b3aea4e80c7ce120
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-03 15:35:40 +0200

    Just test PyQt5 component for now

--- voila_qt_full.py
+++ voila_qt_full.py
@@ -1,20 +1,20 @@
-from multiprocessing import Process
+# from multiprocessing import Process
 
-from voila.app import main
+# from voila.app import main
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from PyQt5 import QtCore
 
 # Note: can't use voila in a thread, so need to use a process
-voila_process = Process(target=main, args=[['basics.ipynb', '--no-browser']])
-voila_process.start()
+# voila_process = Process(target=main, args=[['basics.ipynb', '--no-browser']])
+# voila_process.start()
 
 app = QApplication([''])
 web = QWebEngineView()
 page = QWebEnginePage()
 page.setView(web)
 web.setPage(page)
-web.setUrl(QtCore.QUrl('http://localhost:8866/'))
+web.setUrl(QtCore.QUrl('http://www.google.com/'))
 web.show()
 app.exec_()
