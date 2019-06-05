9bf90c59ccd4800ea773ed597e425e5f6cbb5a66
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-05 10:32:11 +0200

    Fix missing import

--- hooks/hook-voila.py
+++ hooks/hook-voila.py
@@ -1,3 +1,3 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 datas = collect_data_files('voila')
 hiddenimports = collect_submodules('voila')
