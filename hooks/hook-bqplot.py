0199adaf96e0daeb13b149007e151391d18101e4
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-05 10:19:55 +0200

    Make sure bqplot includes all submodules

--- hooks/hook-bqplot.py
+++ hooks/hook-bqplot.py
@@ -1,2 +1,3 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 datas = collect_data_files('bqplot')
+hiddenimports = collect_submodules('bqplot')
